3ba4dee7e084c53c47f921e41769cccf46368974
Dayanna8913 <brownypetite2012@hotmail.com>
2021-09-27 12:39:38 -0500

    Nuevos iconos descargar y agregar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
+import logoAgregar from './img/mas-positivo-suma-simbolo-matematico.svg';
+import logoDescargar from './img/download.svg';
+
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoAgregar} className="App-logo-new" alt="logo agregar" />
+        <br />
+        <img src={logoDescargar} className="App-logo-new" alt="logo descargar" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
